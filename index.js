24ab1de12818a702b49f9a6a8cac8f1783fedf9a
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-09-01 18:09:17 -0400

    commit tree 49da61d6a362446ee26d25ad596cffb2c5745386

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(act="roller-skate"){
+    return `This Saturday, I want to ${act}!`
+}
+
+const mondayWork = function(act="go to the office"){
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(flair='*'){
+    return function(adj='special'){
+        return `You are ${flair}${adj}${flair}!`;
+    }
+
+}
